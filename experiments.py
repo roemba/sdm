b6e29353296ff378c54b566b918deadb12c0b8f9
Jelle Vos <j.v.jelle@gmail.com>
2020-11-14 18:07:44 +0100

    Also determine std

--- experiments.py
+++ experiments.py
@@ -1,4 +1,5 @@
 import time
+import statistics
 from typing import List
 
 from client import Client
@@ -24,27 +25,36 @@ class SystemSetup:
 
 
 def evaluate_setup_runtime():
-    experiment_count = 10000
+    experiment_count = 100
+    run_count = 100
     client_counts = [5, 10, 15, 20, 25, 30, 45, 50]
-    print(f"Evaluate setup runtime for client counts {client_counts}, repeating {experiment_count} times.")
+    print(f"Evaluate setup runtime for client counts {client_counts}, performing {experiment_count} of "
+          f"{run_count} runs.")
 
-    # Generate test data
-    test_data: List[List[SystemSetup]] = []
-    for client_count in client_counts:
-        test_data.append([SystemSetup(client_count) for _ in range(experiment_count)])
-
-    # Perform runtime analysis
     runtimes = []
-    for row in test_data:
-        start = time.monotonic()
+    standard_deviations = []
 
-        for system_setup in row:
-            setup(system_setup.consultant, system_setup.clients)
+    for client_count in client_counts:
+        # Generate test data
+        test_data = [[SystemSetup(client_count) for _ in range(run_count)] for _ in range(experiment_count)]
 
-        runtime = (time.monotonic() - start) / experiment_count
-        runtimes.append(runtime * 1000)
+        # Perform runtime analysis
+        experiment_runtimes = []
+
+        for experiment_data in test_data:
+            start = time.monotonic()
+
+            for run_data in experiment_data:
+                setup(run_data.consultant, run_data.clients)
+
+            runtime = (time.monotonic() - start) / run_count * 1000
+            experiment_runtimes.append(runtime)
+
+        runtimes.append(statistics.mean(experiment_runtimes))
+        standard_deviations.append(statistics.stdev(experiment_runtimes))
 
     print(f"Runtimes: {runtimes} ms")
+    print(f"Standard deviations: {standard_deviations} ms")
 
 
 if __name__ == '__main__':
